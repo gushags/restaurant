193a80c2bf15df95751929e9521a5a5f383fec14
gushags <jeff@hagkull.com>
2025-04-03 12:30:49 -0700

    Create button logic for all pages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// index.js
+/* index.js */
 import "./css-reset-josh.css";
 import "./styles.css";
 import "./menu.css";
@@ -6,6 +6,34 @@ import "./about.css";
 
 import { createHomepage } from "./homepage.js";
 import { createMenu } from "./menu.js";
+import { createAbout } from "./about.js";
 
-// createMenu();
-// createHomepage();
+const homeButton = document.querySelector("#home");
+const menuButton = document.querySelector("#menu");
+const aboutButton = document.querySelector("#about");
+
+homeButton.addEventListener("click", () => {
+  clearContent();
+  createHomepage();
+});
+
+menuButton.addEventListener("click", () => {
+  clearContent();
+  createMenu();
+});
+
+aboutButton.addEventListener("click", () => {
+  clearContent();
+  createAbout();
+});
+
+function clearContent() {
+  const content = document.querySelector("#id");
+  // remove all children of #content
+  while (content.firstChild) {
+    content.removeChild(content.lastChild);
+  }
+}
+
+// Initial page write
+createHomepage();
--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,4 @@
-// menu.js
+/* menu.js */
 
 // import images
 import pArugala from "./img/pizza-arugala.jpg";
@@ -9,89 +9,6 @@ import oPeppers from "./img/omelette-peppers.jpg";
 import oSalmon from "./img/omelette-salmon.jpg";
 import pairsWith from "./img/pairs.png";
 
-/*<div id="menu-headline">
-        <h1>Me<span>nu</span></h1>
-      </div>
-      <div id="menu-content">
-        <div class="paired">
-          <div class="p-card">
-            <img
-              src="./img/pizza-arugala.jpg"
-              class="food"
-              alt="Arugala Pizza"
-            />
-            <h3 class="menu-item">The Shepherd</h3>
-            <p>
-              Arugala and goat cheese play nicely with hand-raised heirloom
-              tomatoes. Crust infused with essence of basil. By the slice.
-            </p>
-            <h4>$12</h4>
-          </div>
-          <div class="pair-png"><img src="/src/img/pairs.png" alt="" /></div>
-          <div class="o-card">
-            <img src="./img/omlette-salmon.jpg" alt="Salmon Omelette" />
-            <h3 class="menu-item">Lox of Love</h3>
-            <p>
-              Transport yourself to Paris with a little lox and eggs. Perfectly
-              seasoned, perfectly portioned, basically perfect. Definitely not a
-              pizza. No substitutions.
-            </p>
-            <h4>$18</h4>
-          </div>
-        </div>
-        <div class="paired">
-          <div class="p-card">
-            <img src="./img/pizza-tomato.jpg" class="food" alt="Tomato Pizza" />
-            <h3 class="menu-item">Cairo Noon</h3>
-            <p>
-              It may look like a tomato pizza, but it's riddled with North
-              African spices that will make you forget you ever heard the
-              pedestrian word, "egg." Enjoy by the slice.
-            </p>
-            <h4>$11</h4>
-          </div>
-          <div class="pair-png"><img src="/src/img/pairs.png" alt="" /></div>
-          <div class="o-card">
-            <img src="./img/omlette-peppers.jpg" alt="Pepper omelet" />
-            <h3 class="menu-item">Pepper Palate Cleanser</h3>
-            <p>
-              Certainly a pizza has its place; but when you've finished rinsing
-              the grease off your fingers, please enjoy this light, delicious
-              dish which will have you raising your champagne glass in
-              acknowledgement of the mighty chicken. No substitutions.
-            </p>
-            <h4>$15</h4>
-          </div>
-        </div>
-        <div class="paired">
-          <div class="p-card">
-            <img
-              src="./img/pizza-pepperoni.jpg"
-              class="food"
-              alt="Pepperoni Pizza"
-            />
-            <h3 class="menu-item">The Homerun</h3>
-            <p>
-              There's a reason the pepperoni pizza is the most popular of all
-              time: it's delicious. Try my take on the icon. And pour your glass
-              of champagne on your eggs while enjoying a perfect beer. By the
-              slice.
-            </p>
-            <h4>$13</h4>
-          </div>
-          <div class="pair-png"><img src="/src/img/pairs.png" alt="" /></div>
-          <div class="o-card">
-            <img src="./img/omlette-gravy.jpg" alt="Gravy Omelette" />
-            <h3 class="menu-item">Hardened Arteries</h3>
-            <p>
-              Oh now we're just putting processed meats on the menu?! Fine,
-              please wash down your cancer-meats with this vat of gravy and
-              eggs. No doggie bags. No substitutions.
-            </p>
-            <h4>$25</h4>
-          </div>
-        { </div>
-      </div> */
 function createMenu() {
   const content = document.getElementById("content");
 
